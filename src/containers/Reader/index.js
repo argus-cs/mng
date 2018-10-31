a99047f7b0bfb42c184cfbf91ff12279476e63fc
Argus <augusto_santos55@hotmail.com>
2018-10-31 12:14:58 -0300

    feat: reader pages and components (alpha)

---
+++ src/containers/Reader/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import classNames from 'classnames/bind'
+
+// layout
+import Layout from '../../layouts/Master'
+
+// style
+import s from './Reader.scss'
+
+// bind styles
+let cx = classNames.bind(s)
+
+class Reader extends Component {
+  render() {
+    return(
+      <Layout>
+        
+      </Layout>
+    )
+  }
+}
+
+export default Reader
